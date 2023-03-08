f089bbc428aa6658a8d44b6b9738da9abfddb97f
YANG XI <gowestyang@hotmail.com>
2023-03-09 07:40:17 +0800

    add C# learning

---
+++ ctrader-c#/2023-joe-marini_learning-c#/Exercise Files/Start/Program Flow/WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "";
+
+            // TODO: basic while loop executes while the gate condition is true
+            Console.WriteLine("Basic while() loop:");
+            while (inputStr != "exit") {
+                
+            }
+
+            Console.WriteLine();
+
+            // TODO: the do-while loop always executes at least one time
+            // Console.WriteLine("The  do-while() loop:");
+
+            // Console.WriteLine();
+        }
+    }
+}
---
+++ ctrader-c#/2023-joe-marini_learning-c#/Notes.md
@@ -0,0 +1,10 @@
+# Variables and Data Types
+
+# Operators
+* null-coalescing: `a ?? b`: if a is null, return b, else a
+* null-assign: `x ??= a`: x = a if x is null
+
+# Program Flow
+* `if(){...} else if(){...} else{}`
+    * ternary operator: `<cond> ? <result true> : <result false>`
+    
